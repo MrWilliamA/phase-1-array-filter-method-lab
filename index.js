ed3777bd5c5cc8ccaed124c1708d0cefbcffeccc
MrWilliamA <will.archer@hotmail.com>
2022-03-16 15:17:34 +1100

    fin

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+
+
+
+  const findMatching = (drivers, name) => {
+    return drivers.filter( driver => driver.toLowerCase() === name.toLowerCase() );
+  }
+
+  // or
+
+  //const findMatching = (drivers, name) => {
+    //return drivers.filter( driver => {
+      //return driver.toLowerCase() === name.toLowerCase()
+      // an alternative solution
+     // // driver.toUpperCase() === name.toUpperCase()
+    //})
+  //}
+  
+  //   
+
+  const fuzzyMatch = (drivers, letter) => {
+    return drivers.filter(driver => driver.slice(0,2) === letter);
+  }
+
+const matchName = (drivers, driverName) => {
+
+    return drivers.filter(driver => driver.name === driverName);
+}
